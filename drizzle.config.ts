1820dd1ea688602be09778badb365031e471394d
jeeva-sd <jeevasubash64@gmail.com>
2024-05-15 00:48:14 +0530

    db files changes.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 import { appConfig } from './src/config';
 
 export default {
-    schema: './src/database/sql/testDB/testDB.schema.ts',
+    schema: './src/database/testDB/testDB.schema.ts',
     out: './migrations',
     driver: 'mysql2',
     dbCredentials: {
